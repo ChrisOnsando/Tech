afa531044712ba99551f77b27e8bcfb3c18d6bc1
ChrisOnsando <chrisnemwel@gmail.com>
2022-04-12 13:45:00 +0300

    commits

--- 3.py
+++ 3.py
@@ -1,6 +1,9 @@
 #Write a password generator function 
 import random
-password = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*_?/"
-length_password = int(input("Enter Password Length:"))
-x = "".join(random.sample(password,length_password))
-print(f"Your Random Password is:{x}")
+def generate_random_password():
+    password = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*_?/"
+    length_password = int(input("Enter Password Length:"))
+    x = "".join(random.sample(password,length_password))
+    print(f"Your Random Password is:{x}")
+
+generate_random_password()
--- 5.py
+++ 5.py
@@ -1,3 +1,6 @@
 #Write a python program to remove duplicates from list
-data = ['a', 'b', 'c', 'd', 'a', 'd', 10, 20, 30, 40, 10, 20]
-print(list(dict.fromkeys(data)))
+def my_duplicate():
+    data = ['a', 'b', 'c', 'd', 'a', 'd', 10, 20, 30, 40, 10, 20, 30, 40]
+    print(list(dict.fromkeys(data)))
+
+my_duplicate()
