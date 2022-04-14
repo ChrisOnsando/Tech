6439afbd68c8731137e0c9d4d435d00c31271be8
ChrisOnsando <chrisnemwel@gmail.com>
2022-04-14 10:32:35 +0300

    commits

--- Day2/assign.py
+++ Day2/assign.py
@@ -1,7 +1,11 @@
-# A Python program to check if a number is a perfect square.
+# Functions used to test programs.
 import math
 from math import log, floor
 from re import X
+import requests
+import json
+import statistics
+from requests.exceptions import Timeout
 
 def perfect_square(number):
     root = math.sqrt(number)
@@ -11,3 +15,30 @@ def check_PowerOf_4(n):
     X = log(n) / log(4)
     return X == floor(X)
 
+def read_data(url):
+    response = requests.get(url).text
+    dict_data = json.loads(response)
+    return dict_data
+
+def basic_stats(num_list):
+    mean_list = statistics.mean(num_list)
+    mode_list = statistics.mode(num_list)
+    variance_list = statistics.variance(num_list)
+    median_list = statistics.median(num_list)
+
+    dict_stats = {
+        'mean':mean_list,
+        'mode':mode_list,
+        'variance':variance_list,
+        'median':median_list
+ 
+    }
+    return dict_stats
+
+def timeout_request(url):
+    try:
+        data = requests.get(url, timeout=10)
+        return data.status_code
+    except Timeout:
+        print('Timeout has been raised.')
+    
--- Day2/test.py
+++ Day2/test.py
@@ -1,5 +1,9 @@
 import unittest
+from unittest import result
 import assign
+import requests
+import json
+from requests.exceptions import Timeout
 
 class TestAssign(unittest.TestCase):
 
@@ -8,6 +12,22 @@ class TestAssign(unittest.TestCase):
 
     def test_check_PowerOf_4(self):
         self.assertTrue(assign.check_PowerOf_4(64),None)
+    
+    def test_read_data(self):
+        self.assertTrue(assign.read_data(url="https://catfact.ninja/facts?limit=1&max_length=140"))
+
+    def test_timeout_request(self):
+        self.assertTrue(assign.timeout_request(url="https://uda.ke/site"))
+
+    def test_basic_stats(self):
+        result = assign.basic_stats([1,2,3,4,5])
+        self.assertTrue(result["mean"], [3])
+        self.assertTrue(result["mode"], [1,2,3,4,5])
+        self.assertTrue(result["variance"], [2.5])
+        self.assertTrue(result["median"], 3)
+
+    
+        
 
 
 
