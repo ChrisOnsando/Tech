b041f050d27f87ec03b9063758e21aaa59a64ff7
ChrisOnsando <chrisnemwel@gmail.com>
2022-04-15 12:20:56 +0300

    commits

--- Day 3/3.py
+++ Day 3/3.py
@@ -1,7 +1,12 @@
 # A numpy program to create an array with atleast 5 user input integers and determine the size of the memory occupied by the arrray
 import numpy as np
-z = np.array(["1, 43355, 56633, 2400, 868"])
-print("Array:")
-print(z)
-print("Size of memory occupied:")
-print("%d bytes" % (z.size * z.itemsize))
+def my_array():
+    print("Enter integers:")
+
+    z = np.array(["1, 43355, 56633, 2400, 868"])
+    print("Array:")
+    print(z)
+    print("Size of memory occupied:")
+    print("%d bytes" % (z.size * z.itemsize))
+
+my_array()
