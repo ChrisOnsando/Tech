b6bfe93eb76002b1e46e9ec57650793aaae1bd96
ChrisOnsando <chrisnemwel@gmail.com>
2022-04-19 12:01:55 +0300

    commits

---
+++ Math/main.py
@@ -0,0 +1,31 @@
+import math
+
+# Return cosine of different numbers
+print(math.cos(0.00))
+print(math.cos(-1.23))
+print(math.cos(10))
+print(math.cos(3.1423))
+
+# Return the inverse hyperbolic cosine
+print(math.acosh(7))
+print(math.acosh(56))
+print(math.acosh(2.78))
+
+# Return the arc sine of numbers
+print(math.asin(0.55))
+print(math.asin(0))
+print(math.asin(1))
+
+# Return the arc tangent of numbers
+print(math.atan(0.39))
+
+# Rounds a number upward to its nearest integer
+print(math.ceil(1.4))
+print(math.ceil(5.3))
+print(math.ceil(22.6))
+
+# Rounds a number down to the nearest integer
+print(math.floor(0.6))
+print(math.floor(1.55))
+print(math.floor(-5.5))
+print(math.floor(10.9))
