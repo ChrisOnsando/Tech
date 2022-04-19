f28c18353dfa65c600aed5e39178b345b6bccca7
ChrisOnsando <chrisnemwel@gmail.com>
2022-04-19 12:14:50 +0300

    commits

--- Math/main.py
+++ Math/main.py
@@ -1,3 +1,4 @@
+import cmath
 import math
 
 # Return cosine of different numbers
@@ -29,3 +30,28 @@ print(math.floor(0.6))
 print(math.floor(1.55))
 print(math.floor(-5.5))
 print(math.floor(10.9))
+
+# Find the arc tangent of a complex number
+print(cmath.atan(2 + 3j))
+
+# Compare the closeness of two complex values using relative tolerance
+print(cmath.isclose(10 + 5j, 11.5 + 2j))
+print(cmath.isclose(10+5j,10.01 + 5j))
+
+# Print log value of some given parameters
+print(cmath.log(1 + 1j))
+print(cmath.log(1, 2.5))
+
+# Print base-10 log value of complex numbers
+print(cmath.log10(2 +3j))
+print(cmath.log10(1 + 2j))
+
+# Print phase of some given parameters
+print(cmath.phase(2 + 3j))
+
+# Convert a polar coordinate to rectangular form
+print(cmath.rect(3.15252626152617, 1.2828891889199))
+
+# Returns square root of a complex number
+print(cmath.sqrt(2 + 3j))
+print(cmath.sqrt(15))
