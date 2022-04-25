f86cb3adb23d9820d97e46cf721b29a1447dffd3
ChrisOnsando <chrisnemwel@gmail.com>
2022-04-25 14:13:25 +0300

    commits

--- Lists/main.py
+++ Lists/main.py
@@ -1,7 +1,12 @@
-thisismylistofteams = ["Manchester United" ,"Manchester City" ,"Best teams to support", "In the Premier league" ]
+thisismylistofteams = ["Manchester United" ,"Manchester City", "Bayern Munich", "PSG", "Liverpool" ,"Best teams to support", "In the Premier league" ]
 print(thisismylistofteams)
 print(len(thisismylistofteams))
 print(type(thisismylistofteams))
 print(thisismylistofteams[2])
 print(thisismylistofteams[-1])
 print(thisismylistofteams[0])
+
+if "Manchester City" in thisismylistofteams:
+ print("Yes", "It is in my favourite teams list")
+else:
+ print("Not in my favourite teams list", thisismylistofteams)
