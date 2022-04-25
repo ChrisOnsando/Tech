e298f4dd78c267845f18dabe7fd42adde16bd091
ChrisOnsando <chrisnemwel@gmail.com>
2022-04-25 14:02:06 +0300

    commits

---
+++ Lists/main.py
@@ -0,0 +1,5 @@
+thisismylistofteams = ["Manchester United" ,"Manchester City" ,"Best teams to support", "In the Premier league" ]
+print(thisismylistofteams)
+print(len(thisismylistofteams))
+print(type(thisismylistofteams))
+print(thisismylistofteams[2])
