e59af8b37fc5328f9a05acd83e13eccb7d768b5b
ChrisOnsando <chrisnemwel@gmail.com>
2022-04-26 15:25:32 +0300

    commits

--- Rock, Paper, Scissors/main.py
+++ Rock, Paper, Scissors/main.py
@@ -5,16 +5,16 @@ computers_choice = random.choice(['scissors', 'rock', 'paper'])
 users_choice = input('Do you want to do- rock, paper or scissors? \n')
 
 if computers_choice == users_choice:
-    print('TIE')
+    print('YOU TIE, The COMPUTER HAD CHOSEN ' +computers_choice)
 
 elif users_choice == 'rock' and computers_choice == 'scissors':
-    print('WIN')
+    print('YOU WIN, THE COMPUTER HAD CHOSEN ' +computers_choice)
 
 elif users_choice == 'paper' and computers_choice == 'rock':
-    print('WIN')    
+    print('YOU WIN, THE COMPUTER HAD CHOSEN ' +computers_choice)    
 
 elif users_choice == 'scissors' and computers_choice == 'paper':
-    print('WIN')
+    print('YOU WIN, THE COMPUTER HAD CHOSEN ' +computers_choice)
 
 else:
-    print('You Lose :( Computer Wins')
+    print('NOW YOU LOSE :( COMPUTER WINS IT')
