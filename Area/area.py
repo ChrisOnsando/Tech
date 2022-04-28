22474f66661bf871735a1675cc28c5e3975b01a3
ChrisOnsando <chrisnemwel@gmail.com>
2022-04-28 11:23:18 +0300

    area of different geometric shapes

--- Area/area.py
+++ Area/area.py
@@ -1,4 +1,7 @@
 # Calculating area of a rectangle
+from cmath import pi
+
+
 def area_rectangle(length: float, width: float) -> float:
 
     if length < 0 or width < 0:
@@ -14,4 +17,24 @@ def area_rhombus(diagonal_1: float, diagonal_2: float) -> float:
     if diagonal_1 < 0 or diagonal_2 < 0:
         raise ValueError("area_rhombus() only accepts non-negative values")
     return 1 / 2 * diagonal_1 * diagonal_2
+
 print(f"Rhombus: {area_rhombus (10, 20) = } cm squared")
+
+# Calculating area of circle
+
+def area_circle(radius: float) -> float:
+   pi = 3.142
+   if radius < 0:
+        raise ValueError("area_circle() only accepts non-negative values")
+   return pi * radius**2
+
+print(f"Circle: {area_circle(20) = } cm squared")   
+
+# Calculating area of ellipse
+def area_ellipse(radius_x: float, radius_y: float) -> float:
+
+    if radius_x < 0 or radius_y < 0:
+        raise ValueError("area_ellipse() only accepts non-negative values")
+    return pi * radius_x * radius_y
+
+print(f"Ellipse: {area_ellipse(20,30) = }")    
