9679e9c8616049c386dc040d719820bfdcf4b375
ChrisOnsando <chrisnemwel@gmail.com>
2022-05-01 12:42:55 +0300

    currency converter GUI

--- Project/demo.py
+++ Project/demo.py
@@ -1,12 +1,33 @@
+from cProfile import label
+from cgitb import text
+from logging import root
 from tkinter import *
 from tkinter import ttk
 import requests
 
-convert = Tk()
+root = Tk()
 # Currency converter title
-convert.title("CURRENCY CONVERTER")
+root.title("CURRENCY CONVERTER")
 # Currency converter dimensions height,width
-convert.geometry("600x400")
+root.geometry("600x400")
 
+e1 = Entry(root)
+e1.pack()
 
-convert.mainloop()
+e2 = Entry(root)
+e2.pack()
+
+e3 = Entry(root)
+e3.pack()
+
+def myClick():
+    mylabel1 =label(root)
+    mylabel1.pack()
+
+myButton = Button(root, text = "CONVERT", width=17, command=myClick)
+myButton.pack()
+
+button_quit = Button(root, text = "EXIT PROGRAM", width=17, command=root.quit)
+button_quit.pack()
+
+root.mainloop()
