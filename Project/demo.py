ebeb952d4e8d03f2f4098a4039fd21cbce68352b
ChrisOnsando <chrisnemwel@gmail.com>
2022-05-02 16:03:28 +0300

    Currency Converter GUI

--- Project/demo.py
+++ Project/demo.py
@@ -10,6 +10,19 @@ root.title("CURRENCY CONVERTER")
 # Currency converter dimensions height,width
 root.geometry("600x400")
 
+class Currency_convertor:
+# An empty dict to store the conversion rates.
+
+    rates = {} 
+
+    def __init__(self, url):
+        data = requests.get(url).json()
+  
+# Extracting only the rates from the json data.
+
+        self.rates = data["rates"] 
+  
+  
 e1 = Label(root, text="From Currency")
 e1.pack()
 e1 = Entry(root, bd =5)
@@ -42,5 +55,5 @@ button_exit.pack()
 if __name__ == "__main__":
   
     url = str.__add__('http://data.fixer.io/api/latest?access_key=', "0cb31eb76a29258a165f5e10a7eb79dd")  
-    
+
 root.mainloop()
