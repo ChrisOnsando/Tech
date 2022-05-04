687f14bf21f508f1117b817936f24af99a213b77
ChrisOnsando <chrisnemwel@gmail.com>
2022-05-04 12:39:47 +0300

    Currency Converter GUI

--- Project/demo.py
+++ Project/demo.py
@@ -15,7 +15,7 @@ class Currency_convertor:
 
     rates = {} 
 
-def __init__(self, url):
+    def __init__(self, url):
         data = requests.get(url).json()
   
 # Extracting only the rates from the json data.
@@ -24,7 +24,7 @@ def __init__(self, url):
   
 # A function to do a simple cross multiplication between the amount and the conversion rates.
 
-def convert_currency(self, from_currency, to_currency, amount):
+    def convert_currency(self, from_currency, to_currency, amount):
 
         initial_amount = amount
   
@@ -32,13 +32,28 @@ def convert_currency(self, from_currency, to_currency, amount):
   
         amount = round(amount * self.rates[to_currency])
 
-        #print('{} {} = {} {}'.format(initial_amount, from_currency, amount, to_currency))
+        return('{} {} = {} {}'.format(initial_amount, from_currency, amount, to_currency))
 
-	
+if __name__ == "__main__":
+  
+    url = str.__add__('http://data.fixer.io/api/latest?access_key=', "0cb31eb76a29258a165f5e10a7eb79dd")  
+  
+    c = Currency_convertor(url)
+  
+    #from_currency = input("Enter to Convert FROM which Currency: ")
+  
+    #to_currency = input("Enter to Convert TO which Currency: ")
+  
+   # amount = int(input("Enter the Amount to Convert: "))
+
+#    c.convert(from_currency, to_currency, amount)
+
+ 	
 # the label for from_currency
 from_currency = Label(top,
 				text = "From Currency").place(x = 40,
 										y = 60)
+                                       
 	
 # the label for to_currency
 to_currency = Label(top,
@@ -52,14 +67,6 @@ amount_converted = Label(top,
 					text = "Amount Converted").place(x = 40,
 											y = 190)
 	                                            
-	
-convert_button = Button(top,
-					text = "Convert").place(x = 40,
-											y = 220)
-
-exit_button = Button(top, text = "EXIT PROGRAM", width=17, command=top.quit)
-exit_button.pack()
-		
 from_currency_input_area = Entry(top,
 							width = 20).place(x = 150,
 											y = 60)
@@ -73,13 +80,10 @@ amount_entry_area = Entry(top,
 amount_converted_entry_area = Entry(top,
 								width = 20).place(x = 190,
 												y = 190)
-		
-if __name__ == "__main__":
-  
-    url = str.__add__('http://data.fixer.io/api/latest?access_key=', "0cb31eb76a29258a165f5e10a7eb79dd")  
-  
-    c = Currency_convertor()
+convert_button = Button(top,
+					text = "Convert").place(x = 40,
+											y = 220)   
 
-
-
+exit_button = Button(top, text = "EXIT PROGRAM", width=17, command=top.quit)
+exit_button.pack()                                            	
 top.mainloop()
