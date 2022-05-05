7fd38f91379e4b57ce87e755b49048da21c84cbb
ChrisOnsando <chrisnemwel@gmail.com>
2022-05-05 10:20:34 +0300

    changes

--- Duplicates/dup.py
+++ Duplicates/dup.py
@@ -5,5 +5,5 @@ def remove_duplicate(List):
                 newlist.append(num)
     return newlist
 
-List = [3,3,3,3,1,3,2,6,7,10,10,22,22,1,1,1,4,5] 
+List = [3,3,3,3,1,3,2,6,7,10,10,22,22,1,1,10,2,3,2,2,1,1,1,4,5] 
 print(remove_duplicate(List))
