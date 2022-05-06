465a992d9c80b33baf15eae4a8caa03fcda4f2ce
ChrisOnsando <chrisnemwel@gmail.com>
2022-05-06 11:46:48 +0300

    commits

--- Dict/dict.py
+++ Dict/dict.py
@@ -14,3 +14,21 @@ print(my_new_dict)
 
 my_new_dict2 = {k:v**2 for k,v in random_dict.items() if v % 2 == 0}
 print(my_new_dict2)
+
+my_new_dict3 = {k:v**3 for k,v in random_dict.items() if v % 3 == 0}
+print(my_new_dict3)
+
+my_new_dict4 = {k:v**4 for k,v in random_dict.items() if v % 4 == 0}
+print(my_new_dict4)
+
+my_new_dict5 = {k:v**5 for k,v in random_dict.items() if v % 5 == 0}
+print(my_new_dict5)
+
+my_new_dict6 = {k:v**6 for k,v in random_dict.items() if v % 6 == 0}
+print(my_new_dict6)
+
+my_new_dict7 = {k:v**7 for k,v in random_dict.items() if v % 7 == 0}
+print(my_new_dict7)
+
+my_new_dict8 = {k:v**8 for k,v in random_dict.items() if v % 8 == 0}
+print(my_new_dict8)
