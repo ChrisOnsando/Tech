3db9a6abe481ae71b7a9c9e2ac925d26873d818a
ChrisOnsando <chrisnemwel@gmail.com>
2022-05-06 12:17:50 +0300

    algos

--- Algos/sorting.py
+++ Algos/sorting.py
@@ -1,4 +1,4 @@
-array = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+array = [54, 26, 93, 17, 77, 31, 44, 55, 20, 1]
 
 
 def bubble_sort(nums: list):
@@ -10,4 +10,21 @@ def bubble_sort(nums: list):
 
 print(array)
 bubble_sort(array)
+print(array)
+
+array = [54, 26, 93, 17, 77, 31, 44, 55, 20, 1]
+
+
+def insertion_sort(nums: list):
+    for i in range(1, len(nums), +1):
+        curr: int = nums[i]
+        pos: int = i
+        while pos > 0 and nums[pos - 1] > curr:
+            nums[pos] = nums[pos - 1]
+            pos -= 1
+        nums[pos] = curr
+
+
+print(array)
+insertion_sort(array)
 print(array)
