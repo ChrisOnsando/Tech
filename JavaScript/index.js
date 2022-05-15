9645aec2fa7d72652bcfde83ad1e2e244e3debca
ChrisOnsando <chrisnemwel@gmail.com>
2022-05-15 14:19:52 +0300

    javascript

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,10 +1,10 @@
 // this script used to check if the number is even or odd
 
-const num = 6;
+const num = 19;
 
 const result = (num % 2  != 0) ? "odd" : "even";
 
 // display the result
 
-console.log(`Number is ${result}.`);
+console.log(`Number is the ${result}.`);
 
