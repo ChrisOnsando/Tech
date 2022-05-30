89223e9bfb2c560d29e9c3a1959ee55873f7fe18
ChrisOnsando <chrisnemwel@gmail.com>
2022-05-30 13:19:08 +0300

    Updates

--- Algos/sorting.py
+++ Algos/sorting.py
@@ -6,7 +6,7 @@ def bubble_sort(nums: list):
         for j in range(len(nums) - 1, i, -1):
             if nums[j - 1] > nums[j]:
                 nums[j - 1], nums[j] = nums[j], nums[j - 1]
-
+    return (nums)
 
 print(array)
 bubble_sort(array)
